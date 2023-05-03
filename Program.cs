9e51524b253b3a63c8dd4b502b5bbee7bd922405
GauriDevhadrao <121218022+GauriDevhadrao@users.noreply.github.com>
2023-05-03 15:21:29 +0530

    Sum of all elements of array

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = new int[100];
+            int i, n, sum = 0;
+
+            Console.WriteLine("Find sum of all elements of Array" );
+
+            Console.WriteLine("Input the number of elements stored in array");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Input {0} elements in the array" );
+            for (i = 0; i< n; i++)
+            {
+                Console.WriteLine("element - {0}", i);
+                a[i] = Convert.ToInt32(Console.ReadLine());
+            }
+        for(i = 0; i < n; i++)
+            {
+                sum += a[i];
+            }
+            Console.WriteLine("Sum of all elements stored in array is : {0}" , sum);
+        }
+    }
+}
